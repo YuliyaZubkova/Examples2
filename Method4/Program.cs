b5cec11cad1b990c7a668602b445c1a0be385171
Yuliya <i@yzubkova.ru>
2023-01-21 23:56:02 +0300

    Method 4 (+ +)

---
+++ Method4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Группа 4 - методы, которые что-то принимают и что-то возвращают.
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result; 
+
+}
+
+string res = Method4(20, "z");
+Console.WriteLine(res);
