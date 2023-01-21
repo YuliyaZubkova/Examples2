2bc9d6fc1543f5a5de3dd644bc83c3ce942f2ce4
Yuliya <i@yzubkova.ru>
2023-01-21 23:19:48 +0300

    Method 2 (+ -)

---
+++ Method2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Метод 2 - что-то принимают, ничего не возвращают
+
+/*void Method2(string msg);
+{
+   Console.WriteLine(msg); 
+}
+Method2(msg: "Текст сообщения"); */
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(count: 6, msg: "New text");
