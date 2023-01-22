69e99e937e84ef1ef175815946e8d7f1e6c43adc
Yuliya <i@yzubkova.ru>
2023-01-22 11:55:42 +0300

    Вывод массива на экран

---
+++ Ex2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Вывод массива на экран несколькими методами - простой, в порядке убывания, в порядке возрастания.
+
+int[] arr = {3, 5, 7, 8, 9, 3, 2};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+void SelectionSortA(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSortA(arr);
+
+PrintArray(arr);
